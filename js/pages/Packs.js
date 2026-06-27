d0ca9b6c458f164795c8f2cdaa493b32d85e33a9
jacobdigma <guyr0820@gmail.com>
2026-06-27 08:19:10 +0200

    Add Sapphire, Ruby, and Quantum packs
    Added three new packs with names, colors, points, and levels.

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,1 +1,32 @@
-
+[
+  {
+    "name": "Sapphire Pack",
+    "color": "#0070ff",
+    "points": 15,
+    "levels": [
+      12345678,
+      23456789,
+      34567890
+    ]
+  },
+  {
+    "name": "Ruby Pack",
+    "color": "#ff0000",
+    "points": 25,
+    "levels": [
+      45678901,
+      56789012
+    ]
+  },
+  {
+    "name": "Quantum Pack",
+    "color": "#00ffcc",
+    "points": 50,
+    "levels": [
+      67890123,
+      78901234,
+      89012345,
+      90123456
+    ]
+  }
+]

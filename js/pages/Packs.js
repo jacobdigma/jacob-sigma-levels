d9246a88720e0f22e8b9db8ffdcc2f84edd8dced
jacobdigma <guyr0820@gmail.com>
2026-07-25 16:32:46 +0200

    Fix return value for level type check

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -75,7 +75,7 @@ export default {
             // Pokud level najdeme, zkontrolujeme jeho typ
             if (foundLevel) {
                 // OPRAVA: Pokud je to Legacy, vrátíme slovo Legacy, jinak klasické číslo pozice #
-                return foundLevel.type === 'legacy' ? 'Legacy' : '' + foundLevel.rank;
+                return foundLevel.type === 'legacy' ? 'Legacy' : '#' + foundLevel.rank;
             }
 
             return "?";

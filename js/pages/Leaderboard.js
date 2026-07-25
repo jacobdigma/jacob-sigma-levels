42048a2554455198e75a0847ba06db2990015cdc
jacobdigma <guyr0820@gmail.com>
2026-07-25 18:50:14 +0200

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -264,11 +264,12 @@ export default {
             });
         });
 
-´    this.rawLeaderboard = Object.values(playersMap).map(player => {
+        this.rawLeaderboard = Object.values(playersMap).map(player => {
             player.stats = `${player.mainCount} Main, ${player.extendedCount} Extended, ${player.legacyCount} Legacy`;
             return player;
      });
-   }, // <--- Přidejte sem chybějící složenou závorku "}"                                      
+   },
+                    // <--- Přidejte sem chybějící složenou závorku "}"                                      
 methods: {
         getLevelStyle(type) {
             if (type === 'main') {

ec6492eeaf16e1d31535824fe4aacb84fa06adc0
jacobdigma <guyr0820@gmail.com>
2026-07-29 20:24:08 +0200

    Fix syntax error in Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -320,6 +320,7 @@ export default {
                 .split('')
                 .map(char =>  127397 + char.charCodeAt(0));
             return String.fromCodePoint(...codePoints);
-        }
+         }
+              }
     }
 };

56857c759151b8ecdfc41bdb5c5982e64cc9ab2c
jacobdigma <guyr0820@gmail.com>
2026-08-03 15:14:27 +0200

    Refactor hardest level assignment logic

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -185,10 +185,11 @@ export default {
                     if (level.type === 'extended') player.extendedCount++;
                     if (level.type === 'legacy') player.legacyCount++;
 
-                    if (level.rank <= player.hardestRank) {
-                        player.hardest = level.name;
-                        player.hardestRank = level.rank;
-                    }
+                    if (level.rank < player.hardestRank || (level.type === 'legacy' && player.hardest === "None")) {
+    player.hardest = level.name;
+    player.hardestRank = level.rank;
+}
+
 
                     const alreadyAdded = player.demons.some(d => d.level === level.name);
                     if (!alreadyAdded) {
@@ -216,10 +217,11 @@ export default {
                             if (level.type === 'extended') player.extendedCount++;
                             if (level.type === 'legacy') player.legacyCount++;
                             
-                        if (level.rank <= player.hardestRank) {
-                                player.hardest = level.name;
-                                player.hardestRank = level.rank;
-                            }
+                       if (level.rank < player.hardestRank || (level.type === 'legacy' && player.hardest === "None")) {
+    player.hardest = level.name;
+    player.hardestRank = level.rank;
+}
+
 
                             const alreadyAdded = player.demons.some(d => d.level === level.name);
                             if (!alreadyAdded) {

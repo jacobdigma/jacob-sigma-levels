892cbec11c1ea26e90efc10a058dd97724a5cedb
jacobdigma <guyr0820@gmail.com>
2026-08-25 15:13:17 +0200

    Fix formatting issues in Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -228,6 +228,7 @@ export default {
                             type: level.type,
                             isVerified: false
                     }),
+                                }
                 }
             }
         });

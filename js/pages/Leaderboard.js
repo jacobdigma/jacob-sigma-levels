899bea51cc9e409aef718ce0e9e7737b8f33e5e0
jacobdigma <guyr0820@gmail.com>
2026-08-25 13:57:07 +0200

    Fix indentation in Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -231,6 +231,7 @@ export default {
                     }
                 }
             }
+                    }
         });
 
                     // AUTOMATICKÁ KONTROLA VYTVOŘENÝCH LEVELŮ (DEMONS CREATED)

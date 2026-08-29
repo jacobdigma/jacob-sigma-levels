11d825b649a21002fd4b4213456b3bc594db9e1e
jacobdigma <guyr0820@gmail.com>
2026-08-29 17:32:12 +0200

    Change calculated points fallback value to 200

--- js/pages/List.js
+++ js/pages/List.js
@@ -225,7 +225,7 @@ export default {
                 const position = activeLevels.indexOf(level);
                 const calculatedPoints = totalActive > 1 
                     ? 300 - (position * (100 / (totalActive - 1))) 
-                    : 300;
+                    : 200;
                 level.points = Math.round(calculatedPoints);
             }
         });
